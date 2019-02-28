db15d19d1d2cdbf2f159390f07c5f3612f75792f
Zbinden Boris Adrien <bzbinden@intranet.epfl.ch>
2019-02-28 18:11:01 +0100

    Working basic deformation scripts

--- Assets/Scripts/MeshDeformerInput.cs
+++ Assets/Scripts/MeshDeformerInput.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MeshDeformerInput : MonoBehaviour
 {
 	public float force = 10f;
-    // Start is called before the first frame update
-    void Start()
+	public float forceOffset = 0.1f;
+	// Start is called before the first frame update
+	void Start()
     {
         
     }
@@ -21,5 +22,18 @@ public class NewBehaviourScript : MonoBehaviour
 
 	void HandleInput () {
 		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(inputRay, out hit))
+		{
+			MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
+			if (deformer)
+			{
+				Vector3 point = hit.point;
+				point += hit.normal * forceOffset;
+				deformer.AddDeformingForce(point, force);
+			}
+		}
 	}
+
 }
