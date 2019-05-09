1b39bf624c6a0cbfa5394f3a565a45135cca4fbb
raybloo <raybloo@ymail.com>
2019-05-09 17:52:10 +0200

    Changed graph width and flipped it, Added massage logs

--- Assets/Scripts/GraphDrawer.cs
+++ Assets/Scripts/GraphDrawer.cs
@@ -1,25 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
+
 
 public class GraphDrawer : MonoBehaviour
 {
 
     // Public Fields
-    
     public Vector3 origin = new Vector3(-2.5f, 1.5f, -0.01f);
     public float widthFactor = 2.0f;
     public float widthLimit = 6.0f;
     public float heightFactor = 0.6f;
     public float thresholdValue;
     public LineRenderer threshold;
+    public string LogPath = "Assets/Logs/massageLog.csv"; 
 
     // Private Fields
     private LineRenderer lineRenderer;
-    private int maxSize = 1000;
+    private int maxSize = 10000;
     private int size;
     private Vector3[] pos;
-    
+    StreamWriter logWriter;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +35,7 @@ public class GraphDrawer : MonoBehaviour
             threshold.positionCount = 2;
             threshold.SetPositions(t_pos);
         }
+        logWriter = new StreamWriter(LogPath,false);
     }
 
     // Update is called once per frame
@@ -39,46 +44,56 @@ public class GraphDrawer : MonoBehaviour
         //UpdateGraph(Mathf.Sin(Time.time),Time.deltaTime);
     }
 
-    void AddPoint(Vector3 point) 
+    private void OnApplicationQuit() {
+        logWriter.Close();
+        AssetDatabase.ImportAsset(LogPath);
+    }
+
+    void AddPoint(Vector3 point,float offset) 
     {
-        if(size >= maxSize) {
-            for(int i = 1; i < maxSize; i++) {
-                pos[i - 1] = pos[i];
-            }
-        } else {
+        if (size < maxSize) {
             size++;
         }
-        pos[size - 1] = point;
+        MoveGraph(offset);
+        for (int i = size - 1; i > 0; i--) {
+            pos[i] = pos[i - 1];
+        }
+        pos[0] = point;
     }
 
     void MoveGraph(float offset) 
     {
         //size = Mathf.Min(maxSize, size);
         for(int i = 0; i < size; i++) {
-            pos[i].x += offset*widthFactor;
-            if(pos[i].x > origin.x+widthLimit) {
-                maxSize = size;
+            pos[i].x -= offset*widthFactor;
+            if(pos[i].x < origin.x-widthLimit) {
+                size--;
             }
         }
-        size = Mathf.Min(maxSize, size);
-        lineRenderer.positionCount = size;
-        lineRenderer.SetPositions(pos);
     }
 
     public void UpdateGraph(float y, float x) 
     {
-        MoveGraph(x);
-        AddPoint(new Vector3(origin.x,origin.y-(y*heightFactor),origin.z));
+        //MoveGraph(x);
+        AddPoint(new Vector3(origin.x,origin.y-(y*heightFactor),origin.z),x);
+        //size = Mathf.Min(maxSize, size);
+        lineRenderer.positionCount = size;
+        lineRenderer.SetPositions(pos);
+        Log(y);
     }
 
     public void UpdateThreshold(float newVal) 
     {
         thresholdValue = newVal;
         if (threshold) {
-            Vector3[] t_pos = new Vector3[] { new Vector3(origin.x, origin.y - (thresholdValue * heightFactor), origin.z), new Vector3(origin.x + widthLimit, origin.y - (thresholdValue * heightFactor), origin.z) };
+            Vector3[] t_pos = new Vector3[] { new Vector3(origin.x, origin.y - (thresholdValue * heightFactor), origin.z), new Vector3(origin.x - widthLimit, origin.y - (thresholdValue * heightFactor), origin.z) };
             threshold.positionCount = 2;
             threshold.SetPositions(t_pos);
         }
+    }
+
+    public void Log(float value) {
+        logWriter.WriteLine(Time.time+","+value);
     }
 
 }
